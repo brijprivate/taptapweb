113d153798ccb437eee81cefad195b91ac746dfa
brijprivate <brijbcet@gmail.com>
2019-05-03 21:09:51 +0530

    a

--- src/app/newlogin/newlogin.component.ts
+++ src/app/newlogin/newlogin.component.ts
@@ -8,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class NewloginComponent implements OnInit {
 
   constructor() { 
-    document.querySelector('.img__btn').addEventListener('click', function() {
-      document.querySelector('.cont').classList.toggle('s--signup');
-    });
+    // document.querySelector('.img__btn').addEventListener('click', function() {
+    //   document.querySelector('.cont').classList.toggle('s--signup');
+    // });
   }
 
   ngOnInit() {
