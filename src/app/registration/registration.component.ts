f86a70d8f0ce50da5f8bf8608e37a2763eca8e88
brijprivate <brijbcet@gmail.com>
2019-05-04 22:18:17 +0530

    a

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -6,9 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-
+public show :boolean =true;
+public hide :boolean =false;
   constructor() { }
-
+  register(){
+    console.log("hi");
+    this.show=false;
+    this.hide=true;
+  }
   ngOnInit() {
   }
 
